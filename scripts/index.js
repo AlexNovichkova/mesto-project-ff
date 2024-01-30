55e359db44dc7d3caf11302c56f739639af08b31
4218 Новичкова А.В. <lovedoctorwho12@gmail.com>
2024-01-30 22:15:47 +0300

    Отправка на первое код-ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,35 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+
+function createCard(data) {
+    const cardTemplate = document.querySelector('#card-template');
+    const cardElement = cardTemplate.content.cloneNode(true).querySelector('.card');
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+    
+    deleteButton.addEventListener('click', function () {
+    deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove(); 
+}
+
+function addCardToPage(cardData) {
+    const cardElement = createCard(cardData);
+    placesList.appendChild(cardElement);
+}
+
+initialCards.forEach(addCardToPage);
+
+  
